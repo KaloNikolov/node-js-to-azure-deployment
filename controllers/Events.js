6912639d6d1a534d61a664213ea8973e1211553b
Kaloyan Nikolov <kaloyan.nikolov@telerik.com>
2016-05-27 15:38:20 +0300

    added the event/type endpoint

--- controllers/Events.js
+++ controllers/Events.js
@@ -9,3 +9,7 @@ var Events = require('./EventsService');
 module.exports.eventCollectTrackingIdPOST = function eventCollectTrackingIdPOST (req, res, next) {
   Events.eventCollectTrackingIdPOST(req.swagger.params, res, next);
 };
+
+module.exports.eventTypeGET = function eventTypeGET (req, res, next) {
+  Events.eventTypeGET(req.swagger.params, res, next);
+};
--- controllers/EventsService.js
+++ controllers/EventsService.js
@@ -12,3 +12,45 @@ exports.eventCollectTrackingIdPOST = function(args, res, next) {
   res.end();
 }
 
+exports.eventTypeGET = function(args, res, next) {
+  /**
+   * parameters expected in the args:
+  * pageSize (BigDecimal)
+  * offset (String)
+  * orderBy (String)
+  **/
+  
+  
+  var examples = {};
+  examples['application/json'] = {
+  "total" : 2000,
+  "offset" : 0,
+  "pageSize" : 2,
+  "hits" : [ {
+    "id" : "pageView",
+    "description" : "User has visited a web page",
+    "schema" : {
+      "properties" : {
+        "pageTitle" : {
+          "type" : "string"
+        },
+        "tags" : {
+          "type" : "string"
+        }
+      },
+      "required" : [ ]
+    }
+  } ]
+};
+  
+  if(Object.keys(examples).length > 0) {
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(examples[Object.keys(examples)[0]] || {}, null, 2));
+  }
+  else {
+    res.end();
+  }
+  
+  
+}
+
